fa58c1f0a37e6e78366db24978011b0c221b125d
William Hart <goodwillhart@googlemail.com>
2019-07-16 19:30:11 +0200

    Make minimal example of hang.

--- test/AbstractAlgebra-test.jl
+++ test/AbstractAlgebra-test.jl
@@ -1,17 +1,18 @@
-include("Maps-test.jl")
-include("Groups-test.jl")
-include("NCRings-test.jl")
-include("Rings-test.jl")
-include("Fields-test.jl")
-include("Modules-test.jl")
-include("Benchmark-test.jl")
+function rand_homomorphism(M::AbstractAlgebra.FPModule{T}, vals...) where T <: RingElement
+   F = FreeModule(ZZ, 5)
+   S = MatrixSpace(ZZ, 5, 3)
+   mat = rand(S, vals...)
+   f = ModuleHomomorphism(F, M, mat)
+   gens1 = [rand(F, vals...) for j in 1:3]
+   S, g = Submodule(F, gens1)
+   hom1 = compose(g, f)
+   return S, hom1
+end
 
-function test_all()
-   test_maps()
-   test_groups()
-   test_ncrings()
-   test_rings()
-   test_fields()
-   test_modules()
-   test_benchmarks()
+function test_module()
+
+         M = FreeModule(ZZ, 3)
+         S, f = rand_homomorphism(M, -10:10)
+
+   println("done")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,7 @@
 using AbstractAlgebra
 
-using SparseArrays, LinearAlgebra
-
-if VERSION < v"0.7.0-DEV.2004"
-   using Base.Test
-else
-   using Test
-end
+using Test
 
 include("../test/AbstractAlgebra-test.jl")
 
-test_all()
+test_module()
